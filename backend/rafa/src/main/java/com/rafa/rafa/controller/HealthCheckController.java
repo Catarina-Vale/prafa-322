7a7f653693dc7a182d524ea03f334208c3e98e47
Alexandre-vale <v-alexan@hotmail.com>
2024-05-06 10:36:13 -0300

    Revert "updated /health ep"
    This reverts commit 99c60d946cafb18e7bbea7f008d1715712c82c14.

--- backend/rafa/src/main/java/com/rafa/rafa/controller/HealthCheckController.java
+++ backend/rafa/src/main/java/com/rafa/rafa/controller/HealthCheckController.java
@@ -1,22 +1,15 @@
 package com.rafa.rafa.controller;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthCheckController{
+public class HealthCheckController {
 
     @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
-        Date currentDate = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String formattedDate = dateFormat.format(currentDate);
-        String response = "I am healthy! Current date: " + formattedDate;
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return new ResponseEntity<>("Hello world!", HttpStatus.OK);
     }
 }
